8d0742dce869f555c8d77b952ed9b55ca5a7c5e2
Azamzaidi <azamzaidi352@gmail.com>
2026-02-19 11:44:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function showMessage() {
 document.getElementById("message").innerHTML =
-  "Be Careful while using online Platforms. <br>"Go for tips and tricks to save yourself."</br> + 'check it here: <a "href="tips.html" target=_blank">Tips Page"</a>';
+  "Be Careful while using online Platforms. <br>"Go for tips and tricks to save yourself."</br> + 
+  'check it here: <a "href="tips.html" target=_blank">Tips Page"</a>';
 }
