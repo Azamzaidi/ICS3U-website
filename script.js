66c22c30044028d37d4ae1dc950d3ed039d4bdaa
Azamzaidi <azamzaidi352@gmail.com>
2026-02-19 12:02:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 <script>
 function showMessage() {
-document.getElementById("message").innerHTML =
-  "Be Careful while using online Platforms.<br>" + "Go for tips and tricks to save yourself.<br>" + 
-  'check it here: <a href="https://azamzaidi.github.io/ICS3U-website/tips.html" target=_blank">Tips</a>';
+document.getElementById("message").innerHTML=
+  "Be Careful while using online Platforms.<br>" +
+  "Go for tips and tricks to save yourself.<br>" + 
+  'check it here: <a href="https://azamzaidi.github.io/ICS3U-website/tips.html" target="_blank">Tips Page</a>';
 }
   </script>
