12b4f26c0b9e7e3f5cfee3c19bbcce72db4d7aa6
Azamzaidi <azamzaidi352@gmail.com>
2026-02-19 10:04:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function showMessage () {
 document.getElementById("message").textContent =
   "Be careful online! Always protect your information.";
+  document.getElementById("goToTips").href =tips.html";
 }
