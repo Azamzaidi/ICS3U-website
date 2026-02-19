4a09ab8398d1ed8972e154f630f64ce5013e6592
Azamzaidi <azamzaidi352@gmail.com>
2026-02-19 10:06:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMessage () {
 document.getElementById("message").textContent =
   "Be careful online! Always protect your information.";
-  document.getElementById("goToTips").href =tips.html";
+  document.getElementById("goToTips").href ="https://azamzaidi.github.io/ICS3U-website/tips.html";
 }
